397a9f0b131c696113b6edcee24d9499c28d48ff
SatyajitAutomation <38632063+SatyajitAutomation@users.noreply.github.com>
2018-04-22 23:48:30 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package MavenDemo.MavenDemo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+public class App {
+	
+	@Test
+	public void test(){
+		
+		//System.setProperty("Webdriver.gecko.driver",System.getProperty("User.dir")+"/geckodriver");
+		
+		//WebDriver driver=new FirefoxDriver();
+		//driver.get("https://www.google.co.in");
+		//Thread.sleep(3000);
+		System.out.println("Hello");
+	}
+}
